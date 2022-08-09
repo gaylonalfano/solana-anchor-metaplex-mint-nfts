1d482456282c7da2b5fe4cc9a832262e0b8be41e
gaylonalfano <gaylon.alfano@gmail.com>
2022-08-09 20:57:23 +0800

    Successfully built and deployed to DEVNET. Still working through this and compairing to raw Solana crates vs. Anchor's approach.

--- programs/solana-anchor-metaplex-mint-nfts/src/lib.rs
+++ programs/solana-anchor-metaplex-mint-nfts/src/lib.rs
@@ -26,12 +26,16 @@ pub mod solana_anchor_metaplex_mint_nfts {
         // Invoke a Cross-program Invocation: 
         // NOTE Hits another program by sending required accounts
         // Q: Is this the spl-token create-account <TOKEN_ADDRESS> command?
+        // A: NO! I believe this is the CPI to SystemProgram, which creates
+        // a fresh account and makes the Token Program its owner.
         msg!("1. Creating account for the actual mint (token)...");
         msg!("Mint: {}", &ctx.accounts.mint.key());
         system_program::create_account(
             // NOTE The CpiContext stores the program and Accounts
             CpiContext::new(
                 // NOTE Every CpiContext takes a program ID and instruction
+                // NOTE Program = What program to hit
+                // NOTE Instructions = What instructions to pass to the program
                 // NOTE Everything is AccountInfo in CpiContext
                 // IMPORTANT I believe this is equivalent to AccountInfo[]:
                 // 
@@ -43,13 +47,14 @@ pub mod solana_anchor_metaplex_mint_nfts {
                 ctx.accounts.token_program.to_account_info(),
                 system_program::CreateAccount {
                     // Our wallet is paying to create the mint account
-                    from: ctx.accounts.mint_authority.to_account_info(),
-                    to: ctx.accounts.mint.to_account_info(),
+                    from: ctx.accounts.mint_authority.to_account_info(), // wallet
+                    to: ctx.accounts.mint.to_account_info(), // mint
                 }
             ),
-            10000000,
-            82,
-            &ctx.accounts.token_program.key()
+            // Additional params
+            10000000, // Lamports
+            82, // Size
+            &ctx.accounts.token_program.key() // Owner i.e. Token Program owns the Mint account
         )?;
 
         // Q: Is this the spl-token create-account <TOKEN_ADDRESS> command?
@@ -60,29 +65,34 @@ pub mod solana_anchor_metaplex_mint_nfts {
         token::initialize_mint(
             CpiContext::new(
                 // Q: Do I use to_account_info() or key()?
-                // A: Must use to_account_info() inside CpiContext
-                // NOTE Don't use & references when using to_account_info()
-                // Only use & when referencing Pubkeys
-                ctx.accounts.token_program.to_account_info(), 
+                // A: MUST use to_account_info() inside CpiContext
+                // NOTE Don't use '&' references when using to_account_info()
+                // Only use '&' when referencing Pubkeys
+                ctx.accounts.token_program.to_account_info(), // Pinging Token Program
                 // Q: What about mint_authority account? Where does it go?
                 // A: It's still present, just passed as arg to initialize_mint(),
                 // instead of inside CpiContext. Not 100% sure why...
-                token::InitializeMint {
+                token::InitializeMint { // Instructions
                     mint: ctx.accounts.mint.to_account_info(),
                     rent: ctx.accounts.rent.to_account_info(),
                 },
             ), 
-            0, 
-            &ctx.accounts.mint_authority.key(),
-            Some(&ctx.accounts.mint_authority.key())
+            0, // Decimals
+            &ctx.accounts.mint_authority.key(), // authority
+            Some(&ctx.accounts.mint_authority.key()) // freeze authority
         )?;
 
         // Q: Is this spl-token create-account <TOKEN_ADDRESS> <OWNER_ADDRESS>?
+        // A: Yes, I believe this is more-or-less the equivalent, BUT it's hitting
+        // the Associated Token Program, which hits the main Token Program, which itself
+        // hits the System Program that creates the ATA.
+        // Q: Does the System Program assign the Associated Token Program OR
+        // the Token Program as the OWNER of the ATA? Is there even an Owner to ATAs? Probably...
         // NOTE When running this CLI command, the owner of account is our local keypair account
         // NOTE This create-account command literally adds the token account (token holdings) inside owner's wallet!
         // Q: Is this the Token Metadata Program creating the Metadata Account for the token?
         // A: Don't believe so because this comes later with steps 5 and 6 w/ Metaplex
-        msg!("3. Creating token account for the mint and the wallet...");
+        msg!("3. Creating associated token account for the mint and the wallet...");
         msg!("Token Address: {}", &ctx.accounts.token_account.to_account_info().key());
         associated_token::create(
             CpiContext::new(
@@ -99,18 +109,21 @@ pub mod solana_anchor_metaplex_mint_nfts {
                 }
                 // Q: What about the other args mint_authority, mint_authority, mint?
                 // Q: Why do we pass rent? This is very different from the raw example
+                // I believe since we're creating a new account using System Program,
+                // that it needs the rent to initialize the account, but not 100% sure...
             ),
         )?;
 
         // Q: Is this spl-token mint <TOKEN_ADDRESS> <AMOUNT> <RECIPIENT_ADDRESS>?
-        // A: Yes, this seems right...
+        // A: Yes! This mints (increases supply of Token) and transfers new tokens
+        // to owner's token account (default recipient token address) balance
         msg!("4. Minting token to the token account (i.e. give it 1 for NFT)...");
         msg!("Mint: {}", &ctx.accounts.mint.key());
         msg!("Token Address: {}", &ctx.accounts.token_account.to_account_info().key());
         token::mint_to(
             CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::MintTo {
+                ctx.accounts.token_program.to_account_info(), // Program to ping
+                token::MintTo { // Instructions to pass to program
                     mint: ctx.accounts.mint.to_account_info(),
                     to: ctx.accounts.token_account.to_account_info(),
                     authority: ctx.accounts.mint_authority.to_account_info(),
@@ -119,7 +132,8 @@ pub mod solana_anchor_metaplex_mint_nfts {
                 // NOTE I believe the raw INSTRUCTION corresponds (mostly) to the
                 // Anchor CpiContext. It's not 100%, but seems to be mostly the case
             ), 
-            1
+            // Additonal args
+            1 // amount
         )?;
 
         msg!("5. Creating metadata account...");
@@ -206,7 +220,8 @@ pub struct MintNft<'info> {
     // let token_program = next_account_info(accounts_iter)?;
     // let associated_token_program = next_account_info(accounts_iter)?;
 
-    /// CHECK: We're about to create this with Metaplex inside transaction
+    /// CHECK: We're about to create this with Metaplex inside transaction,
+    /// so we don't need to validate that it exists
     #[account(mut)]
     pub metadata: UncheckedAccount<'info>,
     /// CHECK: We're about to create this with Metaplex inside transaction
@@ -228,7 +243,7 @@ pub struct MintNft<'info> {
     pub token_program: Program<'info, token::Token>,
     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
 
-    // NOTE This is Metaplex's on-program. We're going to use it via CPI to create some metadata
+    // NOTE This is Metaplex's on-chain program. We're going to use it via CPI to create some metadata
     // FIXME I'm running into: Error processing Instruction 0: instruction expected an
     // executable account when trying to anchor test on localnet. It MAY be a
     // problem since I'm trying to run Metaplex's on-chain program, but I may not
